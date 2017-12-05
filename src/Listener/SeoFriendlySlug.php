f11b2afc428de2943b7f888dd33f77edb5ff9f5e
Zeokat <zeokat@gmail.com>
2017-12-05 02:01:50 +0100

    Create SeoFriendlySlug.php

---
+++ src/Listener/SeoFriendlySlug.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * (c) Zeokat
+ */
+ 
+namespace Zeokat\Transliterator\Listener;
+
+use Flarum\Event\DiscussionWillBeSaved;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class SeoFriendlySlug
+{
+
+}
