da6cdf97cf235dfbc3330311a52347b0cc681152
Zeokat <zeokat@gmail.com>
2017-12-05 02:07:15 +0100

    Update SeoFriendlySlug.php

--- src/Listener/SeoFriendlySlug.php
+++ src/Listener/SeoFriendlySlug.php
@@ -11,4 +11,18 @@ use Illuminate\Contracts\Events\Dispatcher;
 class SeoFriendlySlug
 {
 
+    /**
+     * @param Dispatcher $events
+     */
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(DiscussionWillBeSaved::class, [$this, 'createSlug']);
+    }
+
+    /**
+     * @param DiscussionWillBeSaved $event
+     */
+    public function createSlug(DiscussionWillBeSaved $event)
+    {
+    }
 }
